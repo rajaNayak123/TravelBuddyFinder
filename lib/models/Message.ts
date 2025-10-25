e73a8d42f5b4a4625b4d7f82cf6e8d831b3dbbed
rajaNayak123 <nayakraja151@gmail.com>
2025-10-25 16:42:49 +0530

    Add Message model with sender, receiver, content, read status, and timestamps

---
+++ lib/models/Message.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    read:{
+        type: Boolean,
+        default: false,
+    }
+},{timestamps: true});
+
+const Message = mongoose.model("Message", messageSchema) || mongoose.models.Message;
+
+export {Message};
